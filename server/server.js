dde2a060f7d2a24d135b9859cb74de3c99a46bd4
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-08-12 16:48:56 +0300

    fix unable to bind to $PORT

--- server/server.js
+++ server/server.js
@@ -617,7 +617,7 @@ if(process.env.NODE_ENV ==='production'){
 mongoose
   .connect(process.env.API_KEY2 || API_KEY2, { useNewUrlParser: true })
   .then(() => {
-    app.listen(4000, () => {
+    app.listen(process.env.PORT || 4000, () => {
       console.log('Listening on port 4000');
     });
   })
