4a1900bee32b549480285adbc13ae7342a86ab22
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-08-14 17:37:09 +0300

    deploy a standalone server

--- server.js
+++ server.js
@@ -604,13 +604,16 @@ app.put('/api/saccos/:id', (req, res) => {
     });
 });
 
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static('client/build'))
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static('client/build'))
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
+// }
+
+// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
+
 
 // creating a connection to mongoose
 // 'mongodb://localhost/fika-saf
